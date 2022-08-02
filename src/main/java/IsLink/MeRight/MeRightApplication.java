c81ac083ccfebb1bdeaeb0d8a72126145adc5f09
Mingzhi <alswl_1110@naver.com>
2022-08-02 11:05:08 +0900

    project initialize

---
+++ src/main/java/IsLink/MeRight/MeRightApplication.java
@@ -0,0 +1,13 @@
+package IsLink.MeRight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeRightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeRightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IsLink/MeRight/MeRightApplicationTests.java
@@ -0,0 +1,13 @@
+package IsLink.MeRight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeRightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
